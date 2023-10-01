b50d41d3dafd11be1d0037b440a5b8c31a5077ad
Jason <jason.a.oneill@gmail.com>
2023-10-01 18:20:27 +0100

    fixed tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,17 @@
 module.exports = {
   content: ["./**/*.liquid"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      blue: '#262262',
-      lightBlue: '#00B4CD',
-      aliceBlue: '#f0f8ff',
-      fontBlue: '#015A6B',
-      grey: '#383838'
-    },
     extend: {
       opacity: {
          '88' : '0.88'
+      },
+      colors: {
+        transparent: 'transparent',
+        blue: '#262262',
+        lightBlue: '#00B4CD',
+        aliceBlue: '#f0f8ff',
+        fontBlue: '#015A6B',
+        grey: '#383838'
       }
     }
   },
